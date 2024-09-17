c7f814dc372c61991b2478db6a03c14057b5c7c7
Kariuki11 <irungukenneth433@gmail.com>
2024-09-17 15:01:31 +0300

    New changes

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,40 +1,70 @@
 const http = require('http');
-const url = require('url');
-const countStudents = require('./3-read_file_async'); // Assuming 3-read_file_async.js exists
-const path = process.argv[2]; // Get the path of the CSV file from command line arguments
+const { readFile } = require('fs');
 
-// Create the HTTP server
+const hostname = '127.0.0.1';
+const port = 1245;
+
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
+        }
+        resolve(output);
+      }
+    });
+  });
+}
+
 const app = http.createServer((req, res) => {
-    const parsedUrl = url.parse(req.url, true);
-    
-    if (parsedUrl.pathname === '/') {
-        // Handle the root URL
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello Holberton School!');
-    } else if (parsedUrl.pathname === '/students') {
-        // Handle the /students URL
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.write('This is the list of our students\n');
-
-        // Call the countStudents function
-        countStudents(path)
-            .then(() => {
-                res.end();
-            })
-            .catch((error) => {
-                res.write(error.message);
-                res.end();
-            });
-    } else {
-        // Handle unknown paths
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Not Found');
-    }
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(process.argv[2].toString()).then((output) => {
+      const outString = output.slice(0, -1);
+      res.end(outString);
+    }).catch(() => {
+      res.statusCode = 404;
+      res.end('Cannot load the database');
+    });
+  }
 });
 
-// Make the server listen on port 1245
-app.listen(1245, () => {
-    console.log('Server is listening on port 1245');
+app.listen(port, hostname, () => {
 });
 
 module.exports = app;
