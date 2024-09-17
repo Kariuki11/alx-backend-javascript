a3ed221aa28f22eaf5db1c597213b202ae4f6cc6
Kariuki11 <irungukenneth433@gmail.com>
2024-09-17 14:51:12 +0300

    New changes

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,50 +1,42 @@
-const fs = require('fs').promises;
+const { readFile } = require('fs');
 
-function countStudents(path) {
-    return fs.readFile(path, 'utf8')
-        .then((data) => {
-            const lines = data.trim().split('\n').filter(line => line.length > 0);
-
-            if (lines.length === 0) {
-                throw new Error('Cannot load the database');
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (error, data) => {
+      if (error) {
+        reject(Error('Cannot load the database'));
+      } else {
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
             }
-
-            // Remove the first line (header of the CSV)
-            const headers = lines.shift();
-
-            const studentsByField = {};
-            let totalStudents = 0;
-
-            // Loop through each line (student data) and group by field
-            lines.forEach((line) => {
-                const [firstname, lastname, age, field] = line.split(',');
-
-                if (field && firstname) {
-                    // Increment total number of students
-                    totalStudents++;
-
-                    // If field doesn't exist in the object, initialize it
-                    if (!studentsByField[field]) {
-                        studentsByField[field] = [];
-                    }
-
-                    // Add student's first name to the list of students in that field
-                    studentsByField[field].push(firstname);
-                }
-            });
-
-            // Log the total number of students
-            console.log(`Number of students: ${totalStudents}`);
-
-            // Log the number of students and list of names for each field
-            for (const [field, students] of Object.entries(studentsByField)) {
-                console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
             }
-        })
-        .catch((error) => {
-            // Handle file reading errors
-            throw new Error('Cannot load the database');
-        });
+          }
+        }
+        const l = length - 1;
+        console.log(`Number of students: ${l}`);
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
+          }
+        }
+        resolve(data);
+      }
+    });
+  });
 }
 
 module.exports = countStudents;
